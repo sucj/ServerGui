10c2de7ceba26e0d04bf32e10a326ff0963bd422
KKW557 <kevin557557@gmail.com>
2025-07-27 18:13:57 +0800

    1

---
+++ src/main/java/icu/suc/serverguis/ServerGuis.java
@@ -0,0 +1,10 @@
+package icu.suc.serverguis;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ServerGuis implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
